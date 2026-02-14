3d10190f4914148d8fe6db67a73f2123753288e7
Aidan Hurley-Kalici <57423704+Aidanjhk@users.noreply.github.com>
2026-02-14 16:07:00 -0500

    updated comments for the reaction wheel script

--- axisWheelSim.m
+++ axisWheelSim.m
@@ -1,22 +1,21 @@
 function [theta, omega, thetaOld] = axisWheelSim( ...
     theta, omega, thetaOld, ...
     theta_des, dt, wn, zeta, I, Td)
-% INPUT:
-% theta --> current rotation vector
-% omega --> current angular velocity vector
-% thetaOld --> previous rotation vector
-% theta_des --> desired rotation vector
-% dt --> time step
-% wn --> natural undamped frequency
-% zeta --> damping ratio
-% I --> inertia diagonal  
-% Td --> external & disturbance torque
-% 
-% OUTPUT:
-% theta --> new rotation vector
-% omega --> new angular velocity vector
-% thetaOld --> current rotation vector
-
+    % INPUT:
+    % theta --> current rotation vector
+    % omega --> current angular velocity vector
+    % thetaOld --> previous rotation vector
+    % theta_des --> desired rotation vector
+    % dt --> time step
+    % wn --> natural undamped frequency
+    % zeta --> damping ratio
+    % I --> inertia diagonal  
+    % Td --> external & disturbance torque
+    % 
+    % OUTPUT:
+    % theta --> new rotation vector
+    % omega --> new angular velocity vector
+    % thetaOld --> current rotation vector
 
     Ivec = I(:);
     
