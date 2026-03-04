3b43fafd7e48a570939735f36a68bd17268ba0b9
Aidan Hurley-Kalici <57423704+Aidanjhk@users.noreply.github.com>
2026-03-04 11:18:09 -0500

    Update axisWheelTorqueOutput.m
    +calculates control torque on all three axes

--- axisWheelTorqueOutput.m
+++ axisWheelTorqueOutput.m
@@ -9,12 +9,14 @@ function [Torque] = axisWheelTorqueOutput(anglesCurrent,anglesDesired,SatInertia
     Ivec = [SatInertia(1),SatInertia(2),SatInertia(3)];
     wn = 0.01;
     
-    Kf = (wn^2)*Ivec;
-    tau_r = (2*zeta*wn.*Ivec)./Kf;
+    Kf = (wn^2).*Ivec;
+    tau_r = (2*zeta*wn*Ivec)./Kf;
     anglesCurrent(:,k-1)
     theta_e_prev = anglesCurrent(:,k-1) - anglesDesired;
     theta_e = anglesCurrent(:,k) - anglesDesired;
     
-    Tc = -Kf * ( theta_e + (tau_r * (theta_e - theta_e_prev)/dt) );
+    Tc(1) = -Kf(1) * ( theta_e(1) + (tau_r(1) .* (theta_e(1) - theta_e_prev(1))/dt) );
+    Tc(2) = -Kf(2) * ( theta_e(2) + (tau_r(2) .* (theta_e(2) - theta_e_prev(2))/dt) );
+    Tc(3) = -Kf(3) * ( theta_e(3) + (tau_r(3) .* (theta_e(3) - theta_e_prev(3))/dt) );
     Torque = Tc;
 end
