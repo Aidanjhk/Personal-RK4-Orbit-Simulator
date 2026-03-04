c216fba140e61651cb1ab529ba5ea226920a2941
Aidan Hurley-Kalici <57423704+Aidanjhk@users.noreply.github.com>
2026-03-04 11:20:29 -0500

    Update satAttitudeControl.m
    +unified attitude calculation function accounting for reaction wheel motion

--- satAttitudeControl.m
+++ satAttitudeControl.m
@@ -1,5 +1,33 @@
-function omega_dot_new = satAttitudeControl(angularVelSat,angularVelRW,angularAccRW, satInertia,rwInertia, referenceDirections, commandTorque, externalTorque)
- 
-    W = rwInertia;
-    omega_dot_new = -(satInertia^-1)*cross(angularVelSat,satInertia*angularVelSat + W*angularVelRW) + (satInertia^-1)*(externalTorque'-W*angularAccRW')
+function omega_dot_new = satAttitudeControl( ...
+    angularVelSat, angularVelRW, angularAccRW, ...
+    satInertia, rwInertia, referenceDirections, ...
+    commandTorque, externalTorque)
+
+    % Wheel axis matrix (3xN)
+    W = referenceDirections;
+
+    % ---- enforce column vectors ----
+    angularVelSat = angularVelSat(:);
+    angularVelRW  = angularVelRW(:);
+    angularAccRW  = angularAccRW(:);
+    externalTorque = externalTorque(:);
+
+    % ---- make rwInertia usable ----
+    if isvector(rwInertia)
+        Jw = diag(rwInertia(:));   % Nx1 -> NxN
+    else
+        Jw = rwInertia;            % already NxN
+    end
+
+    % ---- total angular momentum ----
+    H = satInertia * angularVelSat + W * (Jw * angularVelRW);
+
+    % ---- reaction torque from wheels ----
+    tau_rw = - W * (Jw * angularAccRW);
+
+    % ---- rigid body dynamics ----
+    omega_dot_new = satInertia \ ( ...
+        externalTorque + tau_rw ...
+        - cross(angularVelSat, H) ...
+    );
 end
